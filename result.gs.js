fa959d0ab40e875747a969e7e5308400a032750e
new-studio-a <roy068204@gmail.com>
2025-03-14 13:03:52 +0530

    Add files via upload

---
+++ result.gs.js
@@ -0,0 +1,19 @@
+﻿
+function data(){
+var a=document.getElementById("n1").value;
+var b=document.getElementById("n2").value;
+
+
+if(a!="Ashok"){
+  alert("invalid usre");
+     return false;
+}
+if(b!="12345"){
+  alert("invalid password");
+     return false;
+}
+
+else{
+   true;
+ } 
+}
